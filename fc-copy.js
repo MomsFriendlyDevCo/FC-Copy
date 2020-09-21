dee7f7d9741978efbb4a4e39c1c1c70eaa2aba86
Matt Carter <m@ttcarter.com>
2020-09-21 15:40:25 +1000

    BUGFIX: Switch to TamperMonkey

--- fc-copy.js
+++ fc-copy.js
@@ -1,15 +1,42 @@
-function fcCopyReference() {
-	if (!$('.item-header-group .number_li .bt_color_box').length) return alert('Cannot find FreedCamp issue within page');
+// ==UserScript==
+// @name         FreedCamp Issue Copier
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  TamperMonkey script for FreedCamp which copies nicely formatted FC Issues.
+// @author       Matt Carter <m@ttcarter.com>
+// @match        https://freedcamp.com/*
+// @grant        none
+// @see          https://github.com/MomsFriendlyDevCo/FC-Copy
+// ==/UserScript==
 
-	var html = ''
-		+ '<a href="' + window.location.href + '">'
-			+ '<strong>'
-				+ $('.item-header-group .number_li .bt_color_box').text()
-			+ '</strong>'
-		+ '</a>'
-		+ ' - '
-		+ $('.item-title-group .bt_view_title').text();
+(function() {
+	'use strict';
+	function fcCopyReference() {
+		if (!$('.item-header-group .number_li .bt_color_box').length) return alert('Cannot find FreedCamp issue within page');
 
-	var htmlBlob = new Blob([html], {type: 'text/html'});
-	navigator.clipboard.write([new ClipboardItem({[htmlBlob.type]: htmlBlob})]);
-}
+		var html = '<div>'
+			+ '<a href="' + window.location.href + '">'
+				+ '<strong>'
+					+ $('.item-header-group .number_li .bt_color_box').text()
+				+ '</strong>'
+			+ '</a>'
+			+ ' - '
+			+ $('.item-title-group .bt_view_title').text()
+		+ '</div>';
+
+		navigator.clipboard.write([
+			new ClipboardItem({
+				'text/plain': new Blob([$(html).text()], {type: 'text/plain'}),
+				'text/html': new Blob(['<strong>Hello</strong> world'], {type: 'text/html'}),
+			})
+		]);
+	}
+
+	if (!$('.item-header-group .number_li .bt_color_box').length) return; // Not a FC issue page
+
+	$('<a class="btn">Copy</a>')
+		.css('height', '37px')
+		.on('click', fcCopyReference)
+		.prependTo($('.item-title-group .prev_next_buttons'))
+
+})();
